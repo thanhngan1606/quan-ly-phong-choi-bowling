3c3499083122bb64e81cfe6b73203b17555bd70e
Pham Minh Dan <phamdanngu937@gmail.com>
2025-10-05 02:05:35 +0700

    update class Lane

--- src/models/Lane.java
+++ src/models/Lane.java
@@ -14,46 +14,5 @@ public class Lane {
         this.gia = gia;
         this.baoTri = baoTri;
     }
-    
-
-    public String getMaLane() {
-        return MaLane;
-    }
-
-    public void setMaLane(String maLane) {
-        MaLane = maLane;
-    }
-
-    public String getTen() {
-        return Ten;
-    }
-
-    public void setTen(String ten) {
-        Ten = ten;
-    }
-
-    public String getGia() {
-        return Gia;
-    }
-
-    public void setGia(String gia) {
-        Gia = gia;
-    }
-
-    public String getTrangThai() {
-        return TrangThai;
-    }
-
-    public void setTrangThai(String trangThai) {
-        TrangThai = trangThai;
-    }
-
-    public String getBaoTri() {
-        return BaoTri;
-    }
-
-    public void setBaoTri(String baoTri) {
-        BaoTri = baoTri;
-    }
 
 }
