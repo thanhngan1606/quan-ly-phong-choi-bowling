e4dd7e720c40d1fe5d4a25c4cde19188f8f69d7b
Pham Minh Dan <phamdanngu937@gmail.com>
2025-10-05 00:09:32 +0700

    add chuc nang quan ly duong bÆowling

--- src/models/Lane.java
+++ src/models/Lane.java
@@ -1,60 +1,33 @@
 package models;
 
 public class Lane {
-    private String MaLane;
-    private String  Ten;
+    private String maLane;
+    private String tenLane;
+    private String trangThai;
+    private double gia;
+    private String baoTri;
 
-    private String Gia;
-    private String TrangThai;
-    private  String BaoTri;
-
-    public Lane() {}
-    public Lane(String MaLane, String Ten, String TrangThai, String BaoTri, String Gia) {
-        this.MaLane = MaLane;
-        this.Ten = Ten;
-        this.TrangThai = TrangThai;
-        this.Gia = Gia;
-        this.BaoTri = BaoTri;
+    public Lane(String maLane, String tenLane, String trangThai, double gia, String baoTri) {
+        this.maLane = maLane;
+        this.tenLane = tenLane;
+        this.trangThai = trangThai;
+        this.gia = gia;
+        this.baoTri = baoTri;
     }
 
-    public String getMaLane() {
-        return MaLane;
-    }
+    public String getMaLane() { return maLane; }
+    public void setMaLane(String maLane) { this.maLane = maLane; }
+    public String getTenLane() { return tenLane; }
+    public void setTenLane(String tenLane) { this.tenLane = tenLane; }
+    public String getTrangThai() { return trangThai; }
+    public void setTrangThai(String trangThai) { this.trangThai = trangThai; }
+    public double getGiaGio() { return gia; }
+    public void setGiaGio(double gia) { this.gia = gia; }
+    public String getBaoTri() { return baoTri; }
+    public void setBaoTri(String baoTri) { this.baoTri = baoTri; }
 
-    public void setMaLane(String maLane) {
-        MaLane = maLane;
+    @Override
+    public String toString() {
+        return maLane + "," + tenLane + "," + trangThai + "," + gia + "," + baoTri;
     }
-
-    public String getTen() {
-        return Ten;
-    }
-
-    public void setTen(String ten) {
-        Ten = ten;
-    }
-
-    public String getGia() {
-        return Gia;
-    }
-
-    public void setGia(String gia) {
-        Gia = gia;
-    }
-
-    public String getTrangThai() {
-        return TrangThai;
-    }
-
-    public void setTrangThai(String trangThai) {
-        TrangThai = trangThai;
-    }
-
-    public String getBaoTri() {
-        return BaoTri;
-    }
-
-    public void setBaoTri(String baoTri) {
-        BaoTri = baoTri;
-    }
-
 }
