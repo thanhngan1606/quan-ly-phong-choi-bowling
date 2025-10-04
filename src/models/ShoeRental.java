770291bafc1bfcf9ef8994194a147271ced83e0a
thanhngan1606 <149795941+thanhngan1606@users.noreply.github.com>
2025-10-05 02:33:00 +0700

    Update ShoeRental.java

--- src/models/ShoeRental.java
+++ src/models/ShoeRental.java
@@ -5,13 +5,14 @@ public class ShoeRental {
     private String maPhien;
     private int size;
     private double gia;
-
+public String trangThai;
     public ShoeRental() {}
-    public ShoeRental(String maThue, String maPhien, int size, double gia) {
+    public ShoeRental(String maThue, String maPhien, int size, double gia, String trangThai) {
         this.maThue = maThue;
         this.maPhien= maPhien;
         this.size = size;
         this.gia = gia;
+        this.trangThai=trangThai;
     }
 
     // Getter / Setter ...
